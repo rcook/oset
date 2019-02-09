8c278b0e873313b17a0b92ecca5fd6bd3b33ecb2
Richard Cook <rcook@rcook.org>
2019-02-08 21:07:50 -0800

    Fix demo program output

--- app/Main.hs
+++ app/Main.hs
@@ -33,27 +33,27 @@ main = do
     -- Create from list
     let s0 :: OSet Int
         s0 = OSet.fromListL [1, 2, 3, 4, 4, 3, 2, 1, -1, -2, -3]
-    print s0 -- outputs: "fromListL [1,2,3,4,-1,-2,-3]"
+    print s0 -- outputs: "fromList [1,2,3,4,-1,-2,-3]"
 
     -- Append
     let s1 = s0 |> 4
-    print s1 -- outputs: "fromListL [1,2,3,4,-1,-2,-3]"
+    print s1 -- outputs: "fromList [1,2,3,4,-1,-2,-3]"
 
     -- Prepend
     let s2 = 4 |< s0
-    print s2 -- outputs: "fromListL [4,1,2,3,-1,-2,-3]"
+    print s2 -- outputs: "fromList [4,1,2,3,-1,-2,-3]"
 
     -- Append
     let s3 = s0 |<> OSet.fromListL [10, 10, 20, 20, 30, 30]
-    print s3 -- outputs: "fromListL [1,2,3,4,-1,-2,-3,10,20,30]"
+    print s3 -- outputs: "fromList [1,2,3,4,-1,-2,-3,10,20,30]"
 
     -- Map (but note that OSet is not a functor)
     let s4 = OSet.map (\x -> x * x) s3
-    print s4 -- outputs: "fromListL [1,4,9,16,100,400,900]"
+    print s4 -- outputs: "fromList [1,4,9,16,100,400,900]"
 
     -- Filter
     let s5 = OSet.filter (>= 100) s4
-    print s5 -- outputs: "fromListL [100,400,900]"
+    print s5 -- outputs: "fromList [100,400,900]"
 
 #ifdef WORKING_PATTERN_SYNONYMS
     -- Pattern matching
